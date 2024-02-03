8ca0dd9f2588babe47087b0b19f24f06b306ed57
s1f10210254 <s1f102102548@iniad.org>
2024-02-03 11:16:06 +0900

    make file

---
+++ FIrebaseSwiftUI/View/login/HelloView.swift
@@ -0,0 +1,8 @@
+//
+//  HelloView.swift
+//  FIrebaseSwiftUI
+//
+//  Created by Hiroki on 2024/02/03.
+//
+
+import Foundation
---
+++ FIrebaseSwiftUI/View/login/SignUpView.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpView.swift
+//  FIrebaseSwiftUI
+//
+//  Created by Hiroki on 2024/02/03.
+//
+
+import Foundation

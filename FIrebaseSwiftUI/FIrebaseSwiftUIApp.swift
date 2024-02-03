231547261cea91e0d7caa01e0a4d64998b8631be
s1f10210254 <s1f102102548@iniad.org>
2024-02-03 10:41:35 +0900

    Initial Commit

---
+++ FIrebaseSwiftUI/FIrebaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIrebaseSwiftUIApp.swift
+//  FIrebaseSwiftUI
+//
+//  Created by Hiroki on 2024/02/03.
+//
+
+import SwiftUI
+
+@main
+struct FIrebaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

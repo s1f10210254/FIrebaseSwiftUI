b558852817b68c305c2e6aba7db410f8c740f45e
s1f10210254 <s1f102102548@iniad.org>
2024-02-03 13:53:48 +0900

    Hello Page

--- FIrebaseSwiftUI/Views/login/HelloView.swift
+++ FIrebaseSwiftUI/Views/login/HelloView.swift
@@ -1,8 +1,56 @@
-//
-//  HelloView.swift
-//  FIrebaseSwiftUI
-//
-//  Created by Hiroki on 2024/02/03.
-//
+import SwiftUI
 
-import Foundation
+struct HelloView: View {
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Hello")
+                    .font(.largeTitle)
+                    .padding()
+
+                Spacer()
+
+                // アプリのイメージに合ったイラストや画像を挿入
+                Image("welcome-image")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 300, height: 300)
+
+                Spacer()
+
+                NavigationLink(destination: SignInView()) {
+                    Text("ログイン")
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                }
+                .padding()
+
+                NavigationLink(destination: SignUpView()) {
+                    Text("新規登録")
+                        .foregroundColor(.white)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(10)
+                }
+                .padding()
+            }
+            .padding()
+        }
+    }
+}
+
+
+
+
+
+struct HelloPage_Previews: PreviewProvider {
+    static var previews: some View {
+        HelloView()
+    }
+}
+
+

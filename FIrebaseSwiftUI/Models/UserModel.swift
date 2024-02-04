e6830a7315733c9ba7e6180abf030fe45a122f4b
s1f10210254 <s1f102102548@iniad.org>
2024-02-04 16:44:35 +0900

    userProfile setup

---
+++ FIrebaseSwiftUI/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  FIrebaseSwiftUI
+//
+//  Created by Hiroki on 2024/02/04.
+//
+
+import Foundation
---
+++ FIrebaseSwiftUI/ViewModels/UserViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserViewModel.swift
+//  FIrebaseSwiftUI
+//
+//  Created by Hiroki on 2024/02/04.
+//
+
+import Foundation
